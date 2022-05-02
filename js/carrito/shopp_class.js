4b22182ef648d5a5ff1e846353f88acd0042d81d
Federico_Silva <f.nahuelsilva@gmail.com>
2022-05-02 18:24:15 -0300

    awww

--- js/carrito/shopp_class.js
+++ js/carrito/shopp_class.js
@@ -8,25 +8,37 @@ class Tienda{
 
     addProducto(producto)
     {   
-      //buscarProducto
-      // map => [{cantidad:1, producto}] = [producto]
-      let mapped= this.productos.map(element=>element.producto);
-      
-      let enCarrito = mapped.find(element=>element.nombre===producto.nombre);
-
-      if(!enCarrito){
-          this.productos.push({cantidad:1, producto});
-      }  
+      let mapear = this.productos.map(element => element.producto);
+      let enStock = this.productos.find (element => element.id === producto.id);
+      if(enStock){
+        let indexed = mapear.ma(element => element.id);
+        let index = indexed.indexOf(producto.id);
+        this.productos[index].cantidad+=1;
+      }
       else{
-          // [producto] => [id]
-          let indexed = mapped.map(element=>element.nombre);
-          // [1,2,3,4,5,6] => indexOf(6) => 5
-          // ["perro", "gato", "paloma"] =>indexOf("gato") => 1
-          // [{id:1, nombre:"hola"}, {id:2, nombre:"bola"}] => indexOf({id:2, nombre:"bola"}) =>-1
-          let index = indexed.indexOf(producto.nombre);
-          this.productos[index].cantidad+=1;
-      }  
-    }    
+        this.productos.push({cantidad:1, producto});
+      }
+    }
+      /*
+        //buscarProducto
+        // map => [{cantidad:1, producto}] = [producto]
+        let mapped= this.productos.map(element=>element.producto);
+        
+        let enCarrito = mapped.find(element=>element.id===producto.id);
+
+        if(!enCarrito){
+            this.productos.push({cantidad:1, producto});
+        }  
+        else{
+            // [producto] => [id]
+            let indexed = mapped.map(element=>element.id);
+            // [1,2,3,4,5,6] => indexOf(6) => 5
+            // ["perro", "gato", "paloma"] =>indexOf("gato") => 1
+            // [{id:1, nombre:"hola"}, {id:2, nombre:"bola"}] => indexOf({id:2, nombre:"bola"}) =>-1
+            let index = indexed.indexOf(producto.id);
+            this.productos[index].cantidad+=1;
+        }      
+        */
 
    //Guardo los datos en el almacenamietno local//
     save(){
