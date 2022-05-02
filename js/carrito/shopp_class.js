edb063aadfe491a0bc35c8dc75b027deab45f0cc
Federico_Silva <f.nahuelsilva@gmail.com>
2022-05-02 18:12:55 -0300

    rewr

--- js/carrito/shopp_class.js
+++ js/carrito/shopp_class.js
@@ -7,26 +7,9 @@ class Tienda{
     //llega el producto encontrado en addtoshopp
 
     addProducto(producto)
-    {   
-      //buscarProducto
-      // map => [{cantidad:1, producto}] = [producto]
-      let mapped= this.productos.map(element=>element.producto);
-      
-      let enCarrito = mapped.find(element=>element.id===producto.id);
-
-      if(!enCarrito){
-          this.productos.push({cantidad:1, producto});
-      }  
-      else{
-          // [producto] => [id]
-          let indexed = mapped.map(element=>element.id);
-          // [1,2,3,4,5,6] => indexOf(6) => 5
-          // ["perro", "gato", "paloma"] =>indexOf("gato") => 1
-          // [{id:1, nombre:"hola"}, {id:2, nombre:"bola"}] => indexOf({id:2, nombre:"bola"}) =>-1
-          let index = indexed.indexOf(producto.id);
-          this.productos[index].cantidad+=1;
-      }  
-    }    
+    { 
+    this.productos.push(producto)  
+    }
 
    //Guardo los datos en el almacenamietno local//
     save(){
