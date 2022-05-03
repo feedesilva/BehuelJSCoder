d2280ead58ae3c0615909444763e051aa66caee8
Federico_Silva <f.nahuelsilva@gmail.com>
2022-05-03 11:19:22 -0300

    mostrar tabla 4

--- js/carrito/carrito.js
+++ js/carrito/carrito.js
@@ -115,7 +115,7 @@ function addtoShopp(idProduct){
 
 const items = document.querySelector("#items");
 function pintarCarrito(){
-  console.log("ver " + Object.values(tienda));
+  console.log("ver ", Object.values(tienda));
 }
 
 //REVISAR///
