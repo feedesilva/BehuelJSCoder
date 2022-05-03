f47b0f0a3be84b02323a2bbeb3607dbd782c7d45
Federico_Silva <f.nahuelsilva@gmail.com>
2022-05-03 00:17:41 -0300

    busqueda 13

--- js/carrito/carrito.js
+++ js/carrito/carrito.js
@@ -128,7 +128,7 @@ function refreshShopp(){
     const {cantidad, nombre, precio} = product// Desestructuracion
     let nodoLi = document.createElement("div");
     nodoLi.innerHTML = "";
-    nodoLi.innerHTML = `${view_shopp} <br> ${cantidad} - ${nombre} - ${precio}   <br> `;
+    nodoLi.innerHTML = `${product(view_shopp)} <br> ${cantidad} - ${nombre} - ${precio}   <br> `;
     newContainer.appendChild(nodoLi);
   })
   container.appendChild(newContainer);
